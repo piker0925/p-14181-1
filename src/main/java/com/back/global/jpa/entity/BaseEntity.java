153318268e66ed617d5ff3f708e53840e39d9cc7
JUNSEO OH <piker0925@gmail.com>
2026-06-18 10:16:54 +0900

    084-2

--- src/main/java/com/back/global/jpa/entity/BaseEntity.java
+++ src/main/java/com/back/global/jpa/entity/BaseEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
@@ -20,6 +22,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
+    @Setter(PROTECTED)
     protected int id;
 
     @CreatedDate
